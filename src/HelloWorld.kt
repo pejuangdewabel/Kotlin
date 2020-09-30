549d4710c6f64a3735dc94230c2878757c8c873f
pejuangdewabel <danu.17bella@gmail.com>
2020-10-01 01:12:33 +0700

    Pengenalan Printout

---
+++ src/HelloWorld.kt
@@ -0,0 +1,9 @@
+fun main() {
+    println("Hello World")
+    println("Belajar Kotlin Dasar")
+    var price: Int = 100
+    var karakter : Char = 'B'
+    println(price)
+    println(karakter)
+    print("bella")
+}
